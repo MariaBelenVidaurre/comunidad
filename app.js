4942fbfc15220dbe1a86c7cadf6a0335e5731b74
Belen <vidaurremariabelen@gmail.com>
2021-11-18 15:32:35 -0300

    estatico y sin acceso

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var abrirpopup=document.getElementById('windowEmergente');
+abrirpopup=document.getElementById('windowEmergente');
 var overley=document.getElementById('overley');
 var popup=document.getElementById('popup');
 var closepopup=document.getElementById('closepopup');
@@ -15,26 +15,27 @@ closepopup.addEventListener('click', function(){
 })
 
 var arrayGuardar = [];
-// Toma los datos de la susripción y quita el pop up si todos los datos son ingresados
+
 function registrar(){
-  arrayGuardar[0] = document.getElementById('nameUsuario').value;
-  arrayGuardar[1]= document.getElementById('mail').value;
-  arrayGuardar[2]= document.getElementById('contra').value;  
-    if(arrayGuardar[0] == ''){
-      alert("Es necesario completar tu nombre");
-      return false;
-    }else if (arrayGuardar[1]== ''){
-      alert("Es necesario completar tu email");
-      return false;
-    } else if (arrayGuardar[2]== ''){
+  var nombre= document.getElementById('nameUsuario').value;
+  var mail= document.getElementById('mail').value;
+  var password= document.getElementById('contra').value;
+  if(nombre == ''){
+    alert("Es necesario completar tu nombre");
+    return false;
+  }else if (mail== ''){
+    alert("Es necesario completar tu email");
+    return false;
+  } else if (password== ''){
       alert("Es necesario completar tu contraseña");
       return false;
-    }
-    else{
-      overley.classList.remove('active');
-    }
+  }else{
+    overley.classList.remove('active');
+    nombre= arrayGuardar.push(document.getElementById('nameUsuario').value);
+    mail= arrayGuardar.push(document.getElementById('mail').value);
+    password= arrayGuardar.push(document.getElementById('contra').value);
+  }  
 }
-
 
 console.log(arrayGuardar)
 
@@ -43,17 +44,20 @@ var adentro=document.getElementById('adentro');
 var ingreso = [];
 
 function go(){
-  ingreso[0]= document.getElementById('pri').value;
-  ingreso[1]= document.getElementById('segu').value;
-  iniciar.addEventListener('click',function(){
-    if(arrayGuardar[1]==ingreso[0] && arrayGuardar[2]==ingreso[1]){
-      iniciar.addEventListener('click',function(){
-        adentro.classList.add('ponele');
-      })
-    }else{
-      alert('Usuario o contraseña incorrecta')
-    }
-  })
-}
+  var eUsuario= ingreso.push(document.getElementById('pri').value);
+  var pUsuario= ingreso.push(document.getElementById('segu').value);
+  if (eUsuario=''){
+    alert("Ingrese su correo");
+    return false;
+  }else if(pUsuario=''){
+    alert("Ingrese su contraseña");
+    return false;
+  }else{
+  if(arrayGuardar[1]==ingreso[0] && arrayGuardar[2]==ingreso[1]){
+    alert('cooorreecto')
+  }else{
+    alert('Usuario o contraseña incorrecta')
+  }
+  }}
 
 console.log(ingreso)
